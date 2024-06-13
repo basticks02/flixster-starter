d716eecce77c7df328d1688abc8a1592f532775f
Praise Ekanem <basticks@meta.com>
2024-06-13 00:53:11 -0700

    Added the like and watched stretch features on each movie card.

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -2,6 +2,8 @@
 import './MovieCard.css'
 import PropTypes from 'prop-types'
 import video from './assets/video.png'
+import { useState } from 'react'
+
 
 const roundedoff = (rating) => {
   return parseFloat(rating).toFixed(1);
@@ -32,14 +34,30 @@ export default function MovieCard({movie, handleOpenModal}) {
       img_src = "https://image.tmdb.org/t/p/w500"+movie_poster;
     }
 
+    const [likeColor, setLikeColor] = useState('grey');
+    const handleLikeClick = (like) => {
+      like.stopPropagation();
+      setLikeColor(prev => prev === 'grey' ? 'red' : 'grey');
+    }
+
+    const [starColor, setStarColor] = useState('grey');
+    const handleStarClick = (watched) => {
+      watched.stopPropagation();
+      setStarColor(prev => prev === 'grey' ? 'rgb(10, 10, 109)' : 'grey');
+    }
+
   return (
     <div className='moviecard' onClick={() => handleOpenModal(movie)}>
 
         <img className='cardimg' src={img_src} />
 
         <div className='info'>
-            <h4>{movie.original_title}</h4>
-            <p className={getRating(movie.vote_average)}> {rating}</p>
+          <h4>{movie.original_title}</h4>
+          <div className='details'>
+              <i style={{color: likeColor}} className='fas fa-heart icon' onClick={handleLikeClick}></i>
+              <i style={{color: starColor}} className="fa-solid fa-eye" onClick={handleStarClick}></i>
+              <p className={getRating(movie.vote_average)}> {rating}</p>
+          </div>
         </div>
     </div>
   )
